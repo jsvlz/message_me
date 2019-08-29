b295dc44d348f94330bcbb6ef08328015db9086d
Jose Veliz <jveliz3821@gmail.com>
2019-08-28 20:21:08 -0400

    Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: ENV["redis://h:p9f3da888cfa5395b364a2dbcbf8ba076370625ba3230e0c66bf844c0e1d6f350@ec2-3-222-144-23.compute-1.amazonaws.com:32659"])
+$redis = Redis.new(url: ENV["REDIS_URL"])
